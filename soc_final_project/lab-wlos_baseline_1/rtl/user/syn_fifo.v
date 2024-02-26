b143c3a9b1ce8d0dcb75b4026b03866af8fdd651
unknown <samuelcool60208@gmail.com>
2024-02-26 12:25:44 +0800

    this is a test

--- soc_final_project/lab-wlos_baseline_1/rtl/user/syn_fifo.v
+++ soc_final_project/lab-wlos_baseline_1/rtl/user/syn_fifo.v
@@ -16,23 +16,20 @@ module syn_fifo #(parameter DEPTH=4, DATA_WIDTH=8) (
       w_ptr <= 0; r_ptr <= 0;
       data_out <= 0;
     end
-  end
-  
-  // To write data to FIFO
-  always@(posedge clk) begin
-    if(w_en & !full)begin
+	
+	// To write data to FIFO
+	if(w_en & !full)begin
       fifo[w_ptr] <= data_in;
       w_ptr <= w_ptr + 1;
     end
-  end
-  
-  // To read data from FIFO
-  always@(posedge clk) begin
-    if(r_en & !empty) begin
+	
+	// To read data from FIFO
+	if(r_en & !empty) begin
       data_out <= fifo[r_ptr];
       r_ptr <= r_ptr + 1;
     end
-  end
+  end 
+
   
   assign full = ((w_ptr+1'b1) == r_ptr);
   assign empty = (w_ptr == r_ptr);
